0d4391ba0b648776b85a603767f500ab6a53a162
zeynep-sena-dogan <zeynepsena2002.d@gmail.com>
2025-10-30 15:57:43 +0300

    fix: update workflow name and streamline deployment steps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sena-dogan.github.io/cv_bumblebee/",
   integrations: [tailwind()],
 });
