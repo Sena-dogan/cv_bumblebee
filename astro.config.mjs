25ce822b66573a3835a67d59ec6796a31c71631f
zeynep-sena-dogan <zeynepsena2002.d@gmail.com>
2025-10-30 16:17:32 +0300

    fix: update site URL and adjust asset paths in configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sena-dogan.github.io/",
+  site: "https://sena-dogan.github.io",
   base: "/cv_bumblebee",
   trailingSlash: "always",
   integrations: [tailwind()],
